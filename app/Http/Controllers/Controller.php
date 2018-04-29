9dee1a608cc11b6c76cb4833fd4b46d96d0f5d88
Ahmed Aliraqi <aliraqi.dev@gmail.com>
2018-04-29 03:20:02 +0200

    add some helpers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,88 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Send a flash message and Redirect to index.
+     *
+     * @param  string $event
+     * @param  string $level
+     * @param  string $resource
+     * @param  string $lang
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectToIndexWithFlash($event = 'created', $level = 'success', $resource = null, $lang = null)
+    {
+        $this->flash($event, $level, $lang);
+
+        return $this->redirectToIndex($resource);
+    }
+
+    /**
+     * Send a flash message.
+     *
+     * @param  string $event
+     * @param  string $level
+     * @param  string $lang
+     *
+     * @return \Laracasts\Flash\FlashNotifier
+     */
+    public function flash($event = 'created', $level = 'success', $lang = null)
+    {
+        if (! $lang) {
+            $lang = $this->getResourceName();
+        }
+
+        return flash(trans($lang.'.messages.'.$event), $level);
+    }
+
+    /**
+     * Redirect to index.
+     *
+     * @param null $resource
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectToIndex($resource = null)
+    {
+        if (! $resource) {
+            $resource = $this->guessResourceName();
+        }
+
+        return redirect()->route("dashboard.$resource.index");
+    }
+
+    public function redirectToBack($resource = null)
+    {
+        if (! $resource) {
+            $resource = $this->guessResourceName();
+        }
+
+        return redirect()->route("create");
+    }
+
+    /**
+     * Guess the resource name of the controller.
+     *
+     * @return string
+     */
+    protected function guessResourceName()
+    {
+        $classBaseName = class_basename(get_class($this));
+
+        return str_plural(snake_case(str_replace('Controller', '', $classBaseName), '-'));
+    }
+
+    /**
+     * The resource name of the controller.
+     *
+     * @return string
+     */
+    protected function getResourceName()
+    {
+        $classBaseName = class_basename(get_class($this));
+
+        return str_plural(snake_case(str_replace('Controller', '', $classBaseName)));
+    }
 }
