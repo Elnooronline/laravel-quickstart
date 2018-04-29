9eec9d08d17f86c5f3df79b9bd30d2b6744c9067
Ahmed Aliraqi <aliraqi.dev@gmail.com>
2018-04-29 08:39:01 +0200

    change redirect to dashboard

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,7 +32,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
