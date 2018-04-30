0a90def1c8a364cb6d9cae0d4832b95dc4964d7d
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-04-30 14:44:56 +0200

    fix sidebar issue

--- resources/assets/js/dashboard.js
+++ resources/assets/js/dashboard.js
@@ -37,3 +37,6 @@ Vue.component('adminlte-notifications-list', require('./components/AdminlteNotif
 const app = new Vue({
     el: '#app'
 });
+
+// Open sidebar dropdwon when active.
+$('.treeview-menu').find('li.active').closest('.treeview-menu').css('display', 'block');
