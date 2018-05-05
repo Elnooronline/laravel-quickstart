4a2d7e565ba6af5722faa30ab75ab8058cafe7d8
Ahmed Aliraqi <aliraqi.dev@gmail.com>
2018-05-05 22:23:52 +0200

    use getResourceName() method insted of resource property

--- app/Models/Presenters/Presenter.php
+++ app/Models/Presenters/Presenter.php
@@ -31,7 +31,7 @@ class Presenter extends LaracastsPresenter
     public function showButton()
     {
         $entity = $this->entity;
-        $resource = $this->entity->resource;
+        $resource = $this->entity->getResourceName();
 
         return new HtmlString(
             view('layouts.dashboard.presenters.list.show', compact('entity', 'resource')
@@ -47,7 +47,7 @@ class Presenter extends LaracastsPresenter
     public function editButton()
     {
         $entity = $this->entity;
-        $resource = $this->entity->resource;
+        $resource = $this->entity->getResourceName();
 
         return new HtmlString(
             view('layouts.dashboard.presenters.list.edit', compact('entity', 'resource')
@@ -63,7 +63,7 @@ class Presenter extends LaracastsPresenter
     public function deleteButton()
     {
         $entity = $this->entity;
-        $resource = $this->entity->resource;
+        $resource = $this->entity->getResourceName();
 
         return new HtmlString(
             view('layouts.dashboard.presenters.list.delete', compact('entity', 'resource'))->render()
@@ -73,7 +73,7 @@ class Presenter extends LaracastsPresenter
     public function controlButton()
     {
         $entity = $this->entity;
-        $resource = $this->entity->resource;
+        $resource = $this->entity->getResourceName();
 
         $present = [
             'show' => $this->displayShowButton,
