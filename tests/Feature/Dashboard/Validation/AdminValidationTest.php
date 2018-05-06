e9d25c3090f31a64e6a465d6d9d4f39a85ceb57d
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-05-06 10:56:35 +0200

    formating

--- tests/Feature/Dashboard/Validation/AdminValidationTest.php
+++ tests/Feature/Dashboard/Validation/AdminValidationTest.php
@@ -14,25 +14,31 @@ class AdminValidationTest extends TestCase
         $response = $this->post(route('dashboard.admins.store'));
         $response->assertSessionHasErrors(['name']); // name field is required
 
-        $response = $this->post(route('dashboard.admins.store'), ['name' => 123]);
-        $response->assertSessionHasErrors(['name']); // name field must be string
+        $cases = [
+            123, // name field must be string
+            str_random(256), // name field must be less than 255 characters
+        ];
 
-        $response = $this->post(route('dashboard.admins.store'), ['name' => str_random(256)]);
-        $response->assertSessionHasErrors(['name']); // name field must be less than 255 characters
+        foreach ($cases as $case) {
+            $response = $this->post(route('dashboard.admins.store'), ['name' => $case]);
+            $response->assertSessionHasErrors(['name']);
+        }
     }
 
     public function test_name_validation_is_update_method()
     {
         $this->be($admin = create(Admin::class));
 
-        $response = $this->put(route('dashboard.admins.update', $admin), ['name' => '']);
-        $response->assertSessionHasErrors(['name']); // name field is required if exists (sometimes)
+        $cases = [
+            '', // name field is required if exists (sometimes)
+            123, // name field must be string
+            str_random(256), // name field must be less than 255 characters
+        ];
 
-        $response = $this->put(route('dashboard.admins.update', $admin), ['name' => 123]);
-        $response->assertSessionHasErrors(['name']); // name field must be string
-
-        $response = $this->put(route('dashboard.admins.update', $admin), ['name' => str_random(256)]);
-        $response->assertSessionHasErrors(['name']); // name field must be less than 255 characters
+        foreach ($cases as $case) {
+            $response = $this->post(route('dashboard.admins.store'), ['name' => $case]);
+            $response->assertSessionHasErrors(['name']);
+        }
     }
 
     public function test_email_validation_in_store_method()
@@ -42,25 +48,33 @@ class AdminValidationTest extends TestCase
         $response = $this->post(route('dashboard.admins.store'));
         $response->assertSessionHasErrors(['email']); // email field is required
 
-        $response = $this->post(route('dashboard.admins.store'), ['email' => $admin->email]);
-        $response->assertSessionHasErrors(['email']); // email field is unique
+        $cases = [
+            $admin->email, // email field is unique
+            'invalid email', // email field is invalid
+            str_random(256).'@test.com', // email field is too long
+        ];
 
-        $response = $this->post(route('dashboard.admins.store'), ['email' => 'invalid email']);
-        $response->assertSessionHasErrors(['email']); // email field is invalid
-
-        $response = $this->post(route('dashboard.admins.store'), ['email' => str_random(256).'@test.com']);
-        $response->assertSessionHasErrors(['email']); // email field is too long
+        foreach ($cases as $case) {
+            $response = $this->post(route('dashboard.admins.store'), ['email' => $case]);
+            $response->assertSessionHasErrors(['email']);
+        }
     }
 
     public function test_email_validation_in_update_method()
     {
         $this->be($admin = create(Admin::class));
 
-        $response = $this->put(route('dashboard.admins.update', $admin), ['email' => '']);
-        $response->assertSessionHasErrors(['email']); // email field is required if exists (sometimes)
+        $cases = [
+            '', // email field is required if exists (sometimes)
+            create(Admin::class)->email, // email field is unique
+            'invalid email', // email field is invalid
+            str_random(256).'@test.com', // email field is too long
+        ];
 
-        $response = $this->put(route('dashboard.admins.update', $admin), ['email' => create(Admin::class)->email]);
-        $response->assertSessionHasErrors(['email']); // email field is unique
+        foreach ($cases as $case) {
+            $response = $this->put(route('dashboard.admins.update', $admin), ['email' => $case]);
+            $response->assertSessionHasErrors(['email']);
+        }
     }
 
     public function test_password_validation_in_store_method()
@@ -70,34 +84,57 @@ class AdminValidationTest extends TestCase
         $response = $this->post(route('dashboard.admins.store'));
         $response->assertSessionHasErrors(['password']); // password field is required
 
-        $response = $this->post(route('dashboard.admins.store'), ['password' => 123456]);
-        $response->assertSessionHasErrors(['password']); // password field is not confirmed
+        $cases = [
+            123456, // password field is not confirmed
+            str_random(256), // password field is too long
+        ];
 
-        $response = $this->post(route('dashboard.admins.store'), ['password' => str_random(256)]);
-        $response->assertSessionHasErrors(['password']); // password field is too long
+        foreach ($cases as $case) {
+            $response = $this->post(route('dashboard.admins.store'), ['password' => $case]);
+            $response->assertSessionHasErrors(['password']);
+        }
     }
 
     public function test_password_validation_in_update_method()
     {
         $this->be($admin = create(Admin::class));
 
-        $response = $this->put(route('dashboard.admins.update', $admin), ['password' => 'secret']);
-        $response->assertSessionHasErrors(['password']); // email field is not confirmed
+        $cases = [
+            'secret', // password field is not confirmed
+            str_random(256), // password field is too long
+        ];
+
+        foreach ($cases as $case) {
+            $response = $this->put(route('dashboard.admins.update', $admin), ['password' => $case]);
+            $response->assertSessionHasErrors(['password']);
+        }
     }
 
     public function test_avatar_validation_in_store_method()
     {
         $this->be(create(Admin::class));
 
-        $response = $this->post(route('dashboard.admins.store'), ['avatar' => 123]);
-        $response->assertSessionHasErrors(['avatar']); // avatar field is invalid image
+        $cases = [
+            123, // avatar field is invalid image
+        ];
+
+        foreach ($cases as $case) {
+            $response = $this->post(route('dashboard.admins.store'), ['avatar' => $case]);
+            $response->assertSessionHasErrors(['avatar']);
+        }
     }
 
     public function test_avatar_validation_in_update_method()
     {
         $this->be($admin = create(Admin::class));
 
-        $response = $this->put(route('dashboard.admins.update', $admin), ['avatar' => 123]);
-        $response->assertSessionHasErrors(['avatar']); // avatar field is invalid image
+        $cases = [
+            123, // avatar field is invalid image
+        ];
+
+        foreach ($cases as $case) {
+            $response = $this->put(route('dashboard.admins.update', $admin), ['avatar' => $case]);
+            $response->assertSessionHasErrors(['avatar']);
+        }
     }
 }
