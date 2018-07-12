a462986fdbabc03cad9ce7cafd016f151b9b8c85
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-07-12 16:51:15 +0200

    add validation provider

---
+++ app/Providers/ValidationServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+
+class ValidationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Check old password
+        Validator::extend('check_hash', function ($attribute, $value, $parameters, $validator) {
+            return Hash::check($value, current($parameters));
+        });
+
+        Validator::extend('base64_image', function ($attribute, $value, $parameters, $validator) {
+            return validate_base64($value, ['png', 'jpg', 'jpeg', 'gif']);
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
--- config/app.php
+++ config/app.php
@@ -184,6 +184,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ValidationServiceProvider::class,
 
     ],
 
