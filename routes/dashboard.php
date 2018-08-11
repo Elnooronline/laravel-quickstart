f395fa51b0cbfb58b1c056a0bbc441623b84b879
Ahmed Al-Shahawi <alshahawi@outlook.com>
2018-08-11 15:31:29 +0200

    Remove necessary imports and fix typos

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,13 +4,14 @@
 |--------------------------------------------------------------------------
 | Dashboard Routes
 |--------------------------------------------------------------------------
-| All routes in this file prefixed to "dashboard".
+| All routes in this file prefixed with "dashboard".
 |
 | For Example:
 | ->  route('dashboard.home')
 | ->  route('dashboard.users.index')
 | ->  route('dashboard.users.show', $user)
 */
+
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('lang/{lang}', 'SettingController@lang')->name('lang');
 
@@ -19,6 +20,5 @@ Route::put('settings', 'SettingController@update')->name('settings.update');
 Route::get('notifications', 'NotificationController@index')->name('notifications');
 
 Route::resource('admins', 'AdminController');
-
 
 Route::resource('users', 'UserController');
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
